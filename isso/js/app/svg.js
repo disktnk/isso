64bb36698c71bceb30220ee5c5feb6b9bf6b36cc
Daisuke Tanaka <duaipp@gmail.com>
2025-07-25 00:53:54 +0900

    fix to add preview and edit icon

--- isso/js/app/svg.js
+++ isso/js/app/svg.js
@@ -1,4 +1,6 @@
 module.exports = {
     "arrow-down": require("app/svg/arrow-down.svg"),
     "arrow-up": require("app/svg/arrow-up.svg"),
+    "edit": require("app/svg/edit.svg"),
+    "eye": require("app/svg/eye.svg"),
 };
